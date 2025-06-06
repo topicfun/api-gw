75a9ff00f10e7c54fed08c26677c61bd10df4f69
Balogh Zoltán <zozi76@freemail.hu>
2025-06-06 09:10:46 +0200

    V2

--- java-backend/src/main/java/hu/alerant/backend/controller/BackendController.java
+++ java-backend/src/main/java/hu/alerant/backend/controller/BackendController.java
@@ -2,21 +2,25 @@ package hu.alerant.backend.controller;
 
 
 import hu.alerant.backend.model.Message;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class BackendController {
 
     @GetMapping("/hello")
     public Message getHelloMessage() {
+        log.info("backend hello call");
         return new Message("Hello from Java Backend API!");
     }
 
     @GetMapping("/data")
     public Message getData() {
+        log.info("backend data call");
         return new Message("This is some data from the backend.");
     }
 }
