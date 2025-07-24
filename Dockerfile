d58c295c36b4b94b12defe971bd2afd4bc9cf0c6
Balogh Zoltán <zozi76@freemail.hu>
2025-07-24 08:45:29 +0200

    test party role V8

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,14 @@
 FROM openresty/openresty:alpine
 
 # Required for the Lua proxy that handles duplicate headers
-RUN apk add --no-cache perl \
-    && opm get ledgetech/lua-resty-http
+#RUN apk add --no-cache perl curl \ && opm get ledgetech/lua-resty-http
 
 COPY nginx/nginx.conf /usr/local/openresty/nginx/conf/nginx.conf
 COPY nginx/log_format.conf /usr/local/openresty/nginx/conf/log_format.conf
 COPY nginx/app_logger.lua /usr/local/openresty/nginx/conf/app_logger.lua
 COPY nginx/app_logger_body.lua /usr/local/openresty/nginx/conf/app_logger_body.lua
 COPY nginx/routes.conf /usr/local/openresty/nginx/route/routes.conf
-COPY nginx/duplicate_header_proxy.lua /usr/local/openresty/nginx/conf/duplicate_header_proxy.lua
+#COPY nginx/duplicate_header_proxy.lua /usr/local/openresty/nginx/conf/duplicate_header_proxy.lua
 
 RUN mkdir -p /var/log/nginx
 
---
+++ local-pf.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo "Port-forwarding munkamenetek indítása a háttérben..."
+echo "A kimenet a logfájlokba lesz átirányítva, vagy elvetve."
+
+# Minden parancs végén az '&' jel háttérbe küldi.
+# A '> /dev/null 2>&1' átirányítja a standard kimenetet és hibakimenetet
+# a /dev/null-ba, így nem jelennek meg a terminálban.
+# Ha szeretnéd látni a kimenetet, de külön fájlba, akkor "> forward_logs.log 2>&1"-re cseréld.
+
+kubectl port-forward service/int-gw-internal-api-gateway 58080:80 > /dev/null 2>&1 & \
+kubectl port-forward service/dxl-sec-id-profile-svc 58081:8080 > /dev/null 2>&1 & \
+kubectl port-forward service/dxl-sgw-sim-svc 58082:8080 > /dev/null 2>&1 & \
+kubectl port-forward service/alb-redis 56379:6379 > /dev/null 2>&1 & \
+kubectl port-forward service/alb-redis 56380:26379 > /dev/null 2>&1 & \
+kubectl port-forward service/alb-dxl-db-rw 15432:5432 > /dev/null 2>&1 & \
+kubectl port-forward service/strapi-sandbox 58083:8080 > /dev/null 2>&1 & \
+kubectl port-forward service/dxl-party-role-tmf669 58084:8080  > /dev/null 2>&1 & \
+kubectl port-forward service/wiremock-service 58085:80  > /dev/null 2>&1 &
+
+#temp-jd-alerant
+#kubectl port-forward service/dxl-party-role-tmf669 58084:8080 -n jd-alb-sit &
+#kubectl port-forward service/dxl-party-role-tmf669 58084:8080 -n jd-alb-sit > /dev/null 2>&1 &
+#kubectl port-forward service/dxl-party-role-tmf669 58085:8080  > /dev/null 2>&1 &
+#kubectl port-forward service/dxl-party-role-tmf669 58086:8080  > /dev/null 2>&1 &
+
+
+echo "indítási parancs:"
+echo "source ./local-pf.sh"
+echo "A port-forwarding parancsok elindultak. A terminál prompt azonnal visszatér."
+echo "A futó háttérfolyamatok listázásához használd a 'jobs' parancsot."
+echo "A folyamatok leállításához használd a 'kill <PID>' parancsot."
+echo "Ne feledd, a terminál bezárása leállíthatja ezeket a folyamatokat, kivéve, ha 'nohup'-pal indítottad őket."
